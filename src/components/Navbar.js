7ab384023ffbafa647e146cbf3c434c1e375dc12
Rob <r.jacobs790@gmail.com>
2021-06-11 15:23:21 -0400

    created the react app, added folder components and added files

---
+++ src/components/Navbar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { SocialIcon } from 'react-social-icons';
+
+export default function Navbar() {
+    return <h1>Navbar</h1>
+}
